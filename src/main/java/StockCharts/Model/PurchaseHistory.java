a11579600b7d1b0fca57c3e9ebd69b544f149cc6
anilb-cool-dev <abhagwat@rocketmail.com>
2020-05-16 18:12:51 -0700

    test

---
+++ src/main/java/StockCharts/Model/PurchaseHistory.java
@@ -0,0 +1,16 @@
+package StockCharts.Model;
+
+public class PurchaseHistory
+{
+    String date;
+
+    public String getDate()
+    {
+        return date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+}
