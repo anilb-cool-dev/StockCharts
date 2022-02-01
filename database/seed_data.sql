d2442be716502ae09f213b945e68d51b1f6c490b
anilb-cool-dev <abhagwat@rocketmail.com>
2022-01-31 22:05:57 -0800

    Update seed_data.sql

--- database/seed_data.sql
+++ database/seed_data.sql
@@ -1,11 +1,11 @@
-insert into price_history (ticker, date, price) values ('AMZN', '2020-02-12', 29.68);
-insert into price_history (ticker, date, price) values ('AMZN', '2020-02-13', 34.58);
-insert into price_history (ticker, date, price) values ('AMZN', '2020-02-14', 39.68);
-insert into price_history (ticker, date, price) values ('AMZN', '2020-02-15', 19.68);
-insert into price_history (ticker, date, price) values ('AMZN', '2020-02-16', 40.68);
-insert into price_history (ticker, date, price) values ('AMZN', '2020-02-17', 69.68);
+insert into price_history (ticker, date, price) values ('ARKF', '2020-02-12', 29.68);
+insert into price_history (ticker, date, price) values ('ARKF', '2020-02-13', 34.58);
+insert into price_history (ticker, date, price) values ('ARKF', '2020-02-14', 39.68);
+insert into price_history (ticker, date, price) values ('ARKF', '2020-02-15', 19.68);
+insert into price_history (ticker, date, price) values ('ARKF', '2020-02-16', 40.68);
+insert into price_history (ticker, date, price) values ('ARKF', '2020-02-17', 69.68);
 
-insert into purchase_history (ticker, date) values ('AMZN', '2020-01-11');
+insert into purchase_history (ticker, date) values ('ARKF', '2020-01-11');
 
 insert into tickers(ticker) values('VT');
 insert into tickers(ticker) values('VTI');
