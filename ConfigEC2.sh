dcb82a521ab95a0a34920264b2b6071010626a20
Bhagwat <anil.bhagwat@sap.com>
2022-10-10 16:10:06 -0700

    commit

--- ConfigEC2.sh
+++ ConfigEC2.sh
@@ -1,9 +1,12 @@
 mkdir java
 mkdir java/stockcharts
-sudo yum install -y git
 cd java/stockcharts
+sudo yum install -y git
 git clone https://github.com/anilb-cool-dev/StockCharts .
-sudo amazon-linux-extras install java-openjdk11
+sudo amazon-linux-extras install -y java-openjdk11
+sudo amazon-linux-extras install -y tomcat9
 sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 sudo yum install -y apache-maven
+mvn package
+sudo service tomcat start
