e050f654bcde70d8888240e6afe570b2bb093c24
Bhagwat <anil.bhagwat@sap.com>
2022-10-17 18:29:07 -0700

    commit

--- ConfigEC2.sh
+++
@@ -1,9 +0,0 @@
-mkdir java
-mkdir java/stockcharts
-cd java/stockcharts
-git clone https://github.com/anilb-cool-dev/StockCharts .
-mvn package
-export dbinstance=$1
-export dbname=$2
-export dbuserid=$3
-export dbpassword=$4
